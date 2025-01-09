1b66e02ae4c6022286a2160f94b90f2e7cefff59
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-01-09 14:04:45 +0100

    new

--- heartAttackYouthVsAdult.py
+++ heartAttackYouthVsAdult.py
@@ -40,46 +40,6 @@ X_scaled = scaler.fit_transform(X_encoded)
 # Split the dataset into training and testing sets
 X_train, X_test , y_train , y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-log_reg = LogisticRegression(max_iter=1000, random_state=42)
-log_reg.fit(X_train, y_train)
-# new dummie datat
-new_data = pd.DataFrame({
-    "State": ["Bavaria"],
-    "Age_Group": ["Adult"],
-    "Year": [2024],
-    "Gender": ["Male"],
-    "BMI": [28.5],
-    "Smoking_Status": ["Non-Smoker"],
-    "Alcohol_Consumption": [3.5],
-    "Physical_Activity_Level": ["High"],
-    "Diet_Quality": ["Good"],
-    "Family_History": [1],
-    "Hypertension": [0],
-    "Cholesterol_Level": [180],
-    "Diabetes": [0],
-    "Urban_Rural": ["Urban"],
-    "Socioeconomic_Status": ["High"],
-    "Air_Pollution_Index": [45.3],
-    "Stress_Level": ["Low"],
-    "Healthcare_Access": ["Good"],
-    "Education_Level": ["Tertiary"],
-    "Employment_Status": ["Employed"],
-    "Region_Heart_Attack_Rate": [8.0]
-})
-
-
-new_data_encoded = pd.get_dummies(new_data, drop_first=True)
-new_data_encoded = new_data_encoded.reindex(columns=X_encoded.columns, fill_value=0)
-new_data_scaled = scaler.transform(new_data_encoded)
-
-new_prediction = log_reg.predict(new_data_scaled)
-
-
-print("Predicted Heart Attack Incidence for new data:", new_prediction)
-
-
-
-
 
 
 
